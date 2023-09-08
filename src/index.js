feae5fa4a4fac82c166d1bc82126627ef3cd57c0
Facuprogram <facundo.exequiel.chavez@gmail.com>
2023-09-07 21:45:27 -0300

    se corrigieron errores

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "../app.js";
 import { connectDB } from "./db.js";
 
 connectDB();
-app.listen(4000)
-console.log('server on port', 4000);
+app.listen(3000)
+console.log('server on port', 3000);
