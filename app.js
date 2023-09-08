fccbcebf977ce8fbb6e7ac8a699afe97ffab2c36
Facuprogram <facundo.exequiel.chavez@gmail.com>
2023-09-07 22:03:03 -0300

    se levanto la base de datos

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import authRoutes from "./src/routes/auth.routes.js";
+
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(authRoutes);
+
+
+
+export default app; 
