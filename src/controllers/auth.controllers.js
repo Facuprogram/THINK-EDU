699ccbfacda9492552981521359969128efc0f72
Facuprogram <facundo.exequiel.chavez@gmail.com>
2023-09-11 18:05:10 -0300

    se agregaron controles para guardar datos del usuario

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -10,8 +10,8 @@ export const register = async (req, res) => {
             password,
         });
     
-      await newUser.save(); 
-      res.send("registrando")
+      const userSaved = await newUser.save(); 
+      res.json("userSaved")
     } catch (error) {
       console.log(error);  
     }
