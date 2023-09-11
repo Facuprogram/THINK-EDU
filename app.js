bca080b3d05a00d7b1200951bdb1a0828b2bcb04
Facuprogram <facundo.exequiel.chavez@gmail.com>
2023-09-11 01:41:57 -0300

    la base de datos ya esta guardando

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ import authRoutes from "./src/routes/auth.routes.js";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
 
-app.use(authRoutes);
-
+app.use("/api", authRoutes);
 
 
 export default app; 
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,22 @@
-export const register = (req, res) => res.send("register");
+import User from '../models/login.js'
+
+export const register = async (req, res) => {
+    console.log(req.body);
+    const {email, password, username} = req.body
+    try {
+      const newUser = new User ({
+            username,
+            email,
+            password,
+        });
+    
+      await newUser.save(); 
+      res.send("registrando")
+    } catch (error) {
+      console.log(error);  
+    }
+   
+}
 
 export const login = (req, res) => res.send("login");
 
