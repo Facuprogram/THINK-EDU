3754d6b9dd33db4554499de00186bf7f7af8ce3d
JoseI33 <jose.imhoff.20@gmail.com>
2025-02-08 21:30:27 -0300

    feat changed user and teacher models so that they have some kind of relation between them, also modified register

--- src/models/teacher.js
+++ src/models/teacher.js
@@ -1,34 +1,13 @@
 import mongoose from "mongoose";
+
 const teacherSchema = new mongoose.Schema({
-
-
-    name: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    currentDay: {
-        type: Date,
-        default: Date.now
-    },
-    contact: [{
-        telephone: Number,
-        mail: String
-    }],
-    id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    nameInstitution: {
-        type: String,
-        required: true
-    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    currentDay: { type: Date, default: Date.now },
+    telefono: { type: String, required: true },
+    nameInstitution: { type: String, required: true },
     address: String
 });
 
-
-export default mongoose.model('Teacher', teacherSchema)
+export default mongoose.model("Teacher", teacherSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    estate: { type: Boolean, default: false },
 
-    username: {
+    // Role (Can be "teacher", "student", etc.)
+/*     role: {
         type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique:true
-    },
-    
-    password: {
-        type: String,
+        enum: ["teacher", "student"],
         required: true
-    }, 
-    estate: {
-        type: Boolean,
-        default: false,
-    }, 
-  
-    },
-    {
-        timestamps: true
-    })
+    }, */
 
-export default mongoose.model('User', userSchema)
+    // If the user is a teacher, link to the Teacher collection
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" }
+}, 
+{ timestamps: true });
 
-
+export default mongoose.model("User", userSchema);
