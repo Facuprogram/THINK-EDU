a9fa77cf9ae8092f89042ee904a4f40bc7aa0de3
Facuprogram <facundo.exequiel.chavez@gmail.com>
2025-03-12 00:09:12 -0300

    Probando cambios en app.use cors

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',  // URL de tu frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true  // Si usas cookies o tokens
-  }));
-
+  origin: [
+      "http://localhost:5173",  // 
+      "https://think-edu-front.vercel.app"  // 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true  // 
+}));
 app.use("/api", authRoutes);
 app.use("/api", studentRoutes);
 app.use("/api", assignmentRoutes);
